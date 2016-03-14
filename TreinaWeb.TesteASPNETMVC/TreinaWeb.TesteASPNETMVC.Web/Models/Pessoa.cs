9628ef361450a1c6a88013355c38aadfbe0fa5dc
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 13:31:54 -0300

    ComponentModel - Part 03

--- TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -12,6 +12,8 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
     {
         [Required(ErrorMessage = "O nome é obrigatório")]
         [DisplayName("Nome da pessoa:")]
+        [MaxLength(50, ErrorMessage ="O nome deve ter no maximo 50 caracteres.")]
+        [MinLength(5, ErrorMessage ="O nome deve ter no minimo 5 caracteres.")]
         public string Nome { get; set; }
 
         [DisplayName("Idade da pessoa:")]
@@ -19,10 +21,13 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
         public int Idade { get; set; }
 
         [DisplayName("Endereço da pessoa:")]
+        [MaxLength(100, ErrorMessage = "O endereço deve ter no máximo 100 caracteres")]
         public string Endereco { get; set; }
 
         [Required(ErrorMessage = "O e-mail da pessoa é obrigatório")]
         [DisplayName("E-Mail da pessoa")]
+        [MaxLength(80, ErrorMessage = "O e-mail deve ter no máximo 20 caracteres")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
 
