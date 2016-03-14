c72c97501d2101c99432bdf1eb1b57f25c558270
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 11:53:17 -0300

    Component Model - Part 2

--- TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,22 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
 {
     public class Pessoa
     {
+        [Required(ErrorMessage = "O nome é obrigatório")]
         [DisplayName("Nome da pessoa:")]
         public string Nome { get; set; }
+
         [DisplayName("Idade da pessoa:")]
+        [Required(ErrorMessage = "O endereço da pessoa é obrigatório")]
         public int Idade { get; set; }
+
         [DisplayName("Endereço da pessoa:")]
         public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O e-mail da pessoa é obrigatório")]
+        [DisplayName("E-Mail da pessoa")]
+        public string Email { get; set; }
+
+
 
     }
 }
