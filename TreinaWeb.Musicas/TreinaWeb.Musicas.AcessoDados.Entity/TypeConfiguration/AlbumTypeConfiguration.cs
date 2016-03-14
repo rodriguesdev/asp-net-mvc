6dd7d8a74771b93172bef2bb4ca32f803e09a982
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 18:09:56 -0300

    Configurando Entity

---
+++ TreinaWeb.Musicas/TreinaWeb.Musicas.AcessoDados.Entity/TypeConfiguration/AlbumTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinaWeb.Musicas.AcessoDados.Entity.TypeConfiguration
+{
+    class AlbumTypeConfiguration
+    {
+    }
+}
