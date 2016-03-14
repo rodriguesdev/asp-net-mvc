a40eb4545c3ff6620fedc9ece8aec9472764e0bc
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 11:36:35 -0300

    Model Binder - Part 2

--- TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Controllers/TesteController.cs
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Controllers/TesteController.cs
@@ -18,8 +18,7 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Controllers
         [HttpPost]
         public ActionResult Index(Pessoa pessoa)
         {
-            ViewBag.PessoaInformada = pessoa;
-            return View("Saudacao");
+            return View("Saudacao",pessoa);
         }
     }
 }
