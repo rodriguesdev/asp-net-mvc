a109cfacf863eaa04b71a950282c8364d489b80f
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 18:11:06 -0300

    Configurando Entity

--- TreinaWeb.Musicas/TreinaWeb.Comum.Entity/TreinaWebAbstractConfig.cs
+++ TreinaWeb.Musicas/TreinaWeb.Comum.Entity/TreinaWebAbstractConfig.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TreinaWeb.Comum.Entity
 {
-    class TreinaWebAbstractConfig
+    public abstract class TreinaWebAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
     {
+        public TreinaWebAbstractConfig()
+        {
+            ConfigurarNomeTabela();
+            ConfigurarCamposTabela();
+            ConfigurarChavePrimaria();
+            ConfigurarChaveEstrangeira();
+        }
+
+        protected abstract void ConfigurarChaveEstrangeira();
+        protected abstract void ConfigurarChavePrimaria();
+        protected abstract void ConfigurarCamposTabela();
+        protected abstract void ConfigurarNomeTabela();
+
     }
 }
