bfad582719347aced21e004f55dc3ecf1a9d32da
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 17:00:51 -0300

    Inclusão da Camada de acesso a dados

--- TreinaWeb.Musicas/TreinaWeb.Musicas.Dominio/Album.cs
+++ TreinaWeb.Musicas/TreinaWeb.Musicas.Dominio/Album.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace TreinaWeb.Musicas.Dominio
 {
-    class Album
+    public class Album
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Ano { get; set; }
+        public string Observacoes { get; set; }
     }
 }
