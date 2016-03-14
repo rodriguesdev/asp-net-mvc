d1f713769b542f949db03e6634f5bddb8d8fb710
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 17:13:57 -0300

    Inclusão do DbContext

---
+++ TreinaWeb.Musicas/TreinaWeb.Musicas.AcessoDados.Entity/Context/MusicasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinaWeb.Musicas.Dominio;
+
+namespace TreinaWeb.Musicas.AcessoDados.Entity.Context
+{
+    public class MusicasDbContext : DbContext
+    {
+        public DbSet<Album> Albuns { get; set; }
+    }
+}
