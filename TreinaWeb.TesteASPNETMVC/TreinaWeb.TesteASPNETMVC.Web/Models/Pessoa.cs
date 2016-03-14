d4c5452c52d3943462bc818a5c17b5f8313fc250
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 16:33:44 -0300

    Começando Bootstrap

--- TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -11,21 +11,21 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
     public class Pessoa
     {
         [Required(ErrorMessage = "O nome é obrigatório")]
-        [DisplayName("Nome da pessoa:")]
+        [DisplayName("Nome:")]
         [MaxLength(50, ErrorMessage ="O nome deve ter no maximo 50 caracteres.")]
         [MinLength(5, ErrorMessage ="O nome deve ter no minimo 5 caracteres.")]
         public string Nome { get; set; }
 
-        [DisplayName("Idade da pessoa:")]
+        [DisplayName("Idade:")]
         [Required(ErrorMessage = "O endereço da pessoa é obrigatório")]
         public int Idade { get; set; }
 
-        [DisplayName("Endereço da pessoa:")]
+        [DisplayName("Endereço:")]
         [MaxLength(100, ErrorMessage = "O endereço deve ter no máximo 100 caracteres")]
         public string Endereco { get; set; }
 
         [Required(ErrorMessage = "O e-mail da pessoa é obrigatório")]
-        [DisplayName("E-Mail da pessoa")]
+        [DisplayName("E-mail:")]
         [MaxLength(80, ErrorMessage = "O e-mail deve ter no máximo 20 caracteres")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
