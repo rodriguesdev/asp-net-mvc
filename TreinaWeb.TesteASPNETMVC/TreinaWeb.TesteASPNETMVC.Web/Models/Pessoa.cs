b716b5e6f01350f32e880736776ad6854692eeba
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-14 11:45:31 -0300

    ComponentModel - Part 1

--- TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC/TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
 {
     public class Pessoa
     {
+        [DisplayName("Nome da pessoa:")]
         public string Nome { get; set; }
+        [DisplayName("Idade da pessoa:")]
         public int Idade { get; set; }
+        [DisplayName("Endereço da pessoa:")]
         public string Endereco { get; set; }
 
     }
