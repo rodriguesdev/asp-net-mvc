34abee5eab421d00a107d23b7dc65b590c954ef3
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-24 10:42:45 -0300

    Implementação ominioParaViewModelProfile

---
+++ TreinaWeb.Musicas/TreinaWeb.Musicas.Web/AutoMapper/DominioParaViewModelProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TreinaWeb.Musicas.Dominio;
+using TreinaWeb.Musicas.Web.ViewModels.Album;
+
+namespace TreinaWeb.Musicas.Web.AutoMapper
+{
+    public class DominioParaViewModelProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Album, AlbumIndexViewModel>();
+        }
+    }
+}
