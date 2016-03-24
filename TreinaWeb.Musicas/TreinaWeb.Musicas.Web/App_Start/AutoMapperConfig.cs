42baac8c6fa3cabdbcf9a096384c05addea7a1c6
leandro.lacr <leandro.lacr@policiacientifica.sp.gov>
2016-03-24 10:46:04 -0300

    AutoMapperConfig

---
+++ TreinaWeb.Musicas/TreinaWeb.Musicas.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TreinaWeb.Musicas.Web.AutoMapper;
+
+namespace TreinaWeb.Musicas.Web.App_Start
+{
+    public static class AutoMapperConfig
+    {
+        public static void Configurar()
+        {
+            Mapper.AddProfile<DominioParaViewModelProfile>();
+        }
+    }
+}
